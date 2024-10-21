c4c031dac824112ca71c7aa055515494bdd31aff
Jeremy <jeremyvillalva66@gmail.com>
2024-10-21 17:37:14 -0400

    added navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive,  RouterOutlet, RouterModule } from '@angular/router';
-import { StorageService } from './services/storage.service';
+import {  RouterModule } from '@angular/router';
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'doodlr';
-  constructor(public storageService:StorageService){}
 }
