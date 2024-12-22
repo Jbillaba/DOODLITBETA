8d49879adb2e721589c80b7ea0258c95f491d047
Jbillaba <Jeremyvillalva66@gmail.com>
2024-12-22 11:36:28 -0500

    changed users profile page to reflect the same look as the other users pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,7 +17,7 @@ export const routes: Routes = [
     { path: 'register', component: RegisterPageComponent },
     { path: 'login', component: LoginPageComponent },
     { path: 'profile', component: DoodlrProfileComponent, canActivate:[AuthGuardService] },
-    { path: 'usersprofile/:username', component: UsersProfileComponent },
+    { path: 'profile/:username', component: UsersProfileComponent },
     { path: 'image', component: ImageuploadComponent },
     { path: '**', component: FourohfourPageComponent }
 ];
