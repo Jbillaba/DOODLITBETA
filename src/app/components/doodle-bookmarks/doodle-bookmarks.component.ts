3c7990e40393c7ab027e6ba4dc08b870a25a69a8
Jbillaba <Jeremyvillalva66@gmail.com>
2025-05-17 10:15:22 -0400

    changes:
    bookmark component now renders the posts on screen, *css not included*

--- src/app/components/doodle-bookmarks/doodle-bookmarks.component.ts
+++ src/app/components/doodle-bookmarks/doodle-bookmarks.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { DoodlrApiService } from '../../services/doodlr-api.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-doodle-bookmarks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './doodle-bookmarks.component.html',
   styleUrl: './doodle-bookmarks.component.css'
 })
 export class DoodleBookmarksComponent {
 constructor (private doodlrApiService: DoodlrApiService){}
 bookmarks: any;
+bookmarksLength: number;
+Type: string = "NULL"
+
 
   ngOnInit(){
     this.grabDoodles()
@@ -20,7 +25,16 @@ bookmarks: any;
   grabDoodles(){
     this.doodlrApiService.currentUserBookmarks().subscribe((response) => {
       this.bookmarks = response;
+      this.bookmarksLength = this.bookmarks.length;
       console.log(this.bookmarks)
     })
   }
+
+  public postYeah(doodlURL: string){
+      return this.doodlrApiService.postyeahs(doodlURL, this.Type).subscribe()
+    }
+
+  public bookmarkDoodle(doodlID: string){
+       return this.doodlrApiService.bookmarkDoodle(doodlID).subscribe()
+      }
 }
